fd7ed876f1cd93c8e9bfaa36a3b74d96bcd11dc6
Megan Honig <meganhonig@gmail.com>
2020-08-01 14:09:28 -0400

    make gameinfo its own component

--- src/components/Pregame.vue
+++ src/components/Pregame.vue
@@ -1,19 +1,5 @@
 <template>
   <div class="d-flex flex-column justify-space-between">
-    <div class="d-flex align-center">
-      <h2>Game <span> {{ game.uuid }}</span></h2>
-      <v-btn 
-        small
-        class="ml-4 black--text"
-        color="secondary"
-        @click="copyGameLink"
-      >
-        <span class="mr-2">Copy game link</span>
-        <v-icon small>
-          {{ copied ? 'fas fa-clipboard-check' : 'fas fa-clipboard' }}
-        </v-icon>
-      </v-btn>
-    </div>
     
     <p class="font-italic">Waiting for players to join...</p>
     
@@ -26,7 +12,7 @@
         class="mb-8"
       />
     </v-form>
-
+    <GameInfo :gameId="game.uuid" />
     <Players :players="players" :localPlayer="localPlayer" />
     <HostControls
       v-if="localPlayer.isHost"
@@ -41,11 +27,12 @@
 
 import HostControls from './game/HostControls'
 import Players from './game/Players'
+import GameInfo from './game/GameInfo'
 import { startGame, updatePlayer } from '../libraries/api'
 
 export default {
   name: 'Pregame',
-  components: { HostControls, Players },
+  components: { HostControls, Players, GameInfo },
   props: {
     players: {
       type: Array,
@@ -70,15 +57,6 @@ export default {
     async startGame () {
       const data = await startGame(this.game.uuid)
       this.$emit('started', data)
-    },
-    copyGameLink () {
-      const el = document.createElement('input')
-      el.value = `${process.env.VUE_APP_PUBLIC_PATH}/game/${this.game.uuid}`
-      document.body.appendChild(el)
-      el.select()
-      document.execCommand('copy')
-      document.body.removeChild(el)
-      this.copied = true
     },
     async updatePlayer () {
       const { data } = updatePlayer(this.localPlayer.uuid, { name: this.name })      
---
+++ src/components/game/GameInfo.vue
@@ -0,0 +1,43 @@
+<template>
+<section class="d-flex align-center">
+      <h2>Game <span> {{ gameId }}</span></h2>
+      <v-btn 
+        small
+        class="ml-4 black--text"
+        color="secondary"
+        @click="copyGameLink"
+      >
+        <span class="mr-2">Copy game link</span>
+        <v-icon small>
+          {{ copied ? 'fas fa-clipboard-check' : 'fas fa-clipboard' }}
+        </v-icon>
+      </v-btn>
+    </section>
+</template>
+
+<script>
+  export default {
+    props: {
+      gameId: {
+        type: String,
+        required: true
+      }
+    },
+    data () {
+      return {
+        copied: false
+      }
+    },
+    methods: {
+      copyGameLink () {
+        const el = document.createElement('input')
+        el.value = `${process.env.VUE_APP_PUBLIC_PATH}/game/${this.gameId}`
+        document.body.appendChild(el)
+        el.select()
+        document.execCommand('copy')
+        document.body.removeChild(el)
+        this.copied = true
+      },
+    }
+  }
+</script>
