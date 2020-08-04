0ff1e2aa899068877e49fbee8a2e7c37400164c7
Megan Honig <meganhonig@gmail.com>
2020-08-03 20:25:23 -0400

    add progress bar to GameInfo, fix undefined prop

--- src/components/game/GameInfo.vue
+++ src/components/game/GameInfo.vue
@@ -1,22 +1,28 @@
 <template>
 <v-col class="elevation-3">
-  <div>
-  <v-subheader>Game {{ game.uuid }} </v-subheader>
-    
+  <v-subheader>Game {{ game.uuid }}</v-subheader>
+  <div class="pa-4 d-flex font-italic justify-space-between align-center">
+    <span>Progress:</span> 
+    <v-progress-linear 
+      class="ml-4" 
+      value="25"
+      color="accent"
+      :height="24"
+    >25%
+    </v-progress-linear>
   </div>
-  <p class="pt-4 px-4 font-italic">{{ status }}</p>
   <div class="d-flex justify-space-between">
   <v-btn 
-      small
-      class="ma-4 black--text"
-      color="secondary"
-      @click="copyGameLink"
-    >
-      <span class="mr-2">Copy game link</span>
-      <v-icon small>
-        {{ copied ? 'fas fa-clipboard-check' : 'fas fa-clipboard' }}
-      </v-icon>
-    </v-btn>
+    small
+    class="ma-4 black--text"
+    color="secondary"
+    @click="copyGameLink"
+  >
+    <span class="mr-2">Copy invite link</span>
+    <v-icon small>
+      {{ copied ? 'fas fa-clipboard-check' : 'fas fa-clipboard' }}
+    </v-icon>
+  </v-btn>
   
   <HostControls 
     v-if="localPlayer.isHost" 
@@ -53,12 +59,15 @@ export default {
         if (this.game.status === 'active') return 'Speaking with the oracle'
         if (this.game.status === 'complete') return 'Game complete!'
         return 'Something is wrong.'
+      },
+      gameLink () {
+        return `${process.env.VUE_APP_PUBLIC_PATH}/game/${this.game.uuid}`
       }
     },
     methods: {
       copyGameLink () {
         const el = document.createElement('input')
-        el.value = `${process.env.VUE_APP_PUBLIC_PATH}/game/${this.gameId}`
+        el.value = this.gameLink
         document.body.appendChild(el)
         el.select()
         document.execCommand('copy')
